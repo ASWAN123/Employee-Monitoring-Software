0f1ec85022a154109d8f41ea82aec20e1718cd98
abderrahim assoaune <aaswan048@gmail.com>
2023-08-07 14:02:44 +0100

    setup gh-pages

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -11,9 +11,16 @@ const firebaseConfig = {
   messagingSenderId:process.env.REACT_APP_E ,
   appId: process.env.REACT_APP_F
   };
-firebase.initializeApp(firebaseConfig) ;
+  
+firebase.initializeApp(firebaseConfig, {
+  experimentalForceLongPolling: true, // this line
+  useFetchStreams: false, // and this line
+}) ;
 
 export const db = firebase.firestore();
-const myapp = initializeApp(firebaseConfig)
+const myapp = initializeApp(firebaseConfig , {
+  experimentalForceLongPolling: true, // this line
+  useFetchStreams: false, // and this line
+})
 const  auth = getAuth(myapp)
 export {auth}
